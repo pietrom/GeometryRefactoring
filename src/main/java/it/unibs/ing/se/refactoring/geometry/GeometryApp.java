3023113284993c5861a4e84878abedad735545be
Pietro Martinelli <amicofragile@gmail.com>
2018-05-22 09:30:48 +0200

    Second E2E test, with output analysis

--- src/main/java/it/unibs/ing/se/refactoring/geometry/GeometryApp.java
+++ src/main/java/it/unibs/ing/se/refactoring/geometry/GeometryApp.java
@@ -11,24 +11,32 @@ public class GeometryApp {
 		this.input = input;
 		this.output = output;
 	}
-	
+
 	public void run() {
 		boolean go = true;
-		while(go) {
+		while (go) {
 			println("Scegli un'azione:");
 			println("0) Uscita");
 			println("1) Lista poligoni");
 			println("2) Aggiunta poligono");
 			int choice = readInt();
+			switch (choice) {
+			case 0:
+				go = false;
+				break;
+			case 1:
+				println("Polygon list:");
+				break;
+			}
 			go = choice != 0;
 		}
 	}
-	
+
 	private void println(String msg) {
 		output.println(msg);
 		output.flush();
 	}
-	
+
 	private int readInt() {
 		String s = input.next();
 		return Integer.parseInt(s);
--- src/test/java/it/unibs/ing/se/refactoring/geometry/GeometryAppTest.java
+++ src/test/java/it/unibs/ing/se/refactoring/geometry/GeometryAppTest.java
@@ -5,6 +5,8 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 
 public class GeometryAppTest {
 	@Test(timeout = 200)
@@ -13,5 +15,16 @@ public class GeometryAppTest {
 		input.useDelimiter("\n");
 		GeometryApp app = new GeometryApp(input, new PrintWriter(new ByteArrayOutputStream()));
 		app.run();
+	}
+	
+	@Test(timeout = 200)
+	public void shouldPrintPolygonListAndExit() throws Exception {
+		Scanner input = new Scanner("1\n0\n");
+		input.useDelimiter("\n");
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		GeometryApp app = new GeometryApp(input, new PrintWriter(output));
+		app.run();
+		String outText = output.toString();
+		assertThat(outText, containsString("Polygon list:"));
 	}
 }
